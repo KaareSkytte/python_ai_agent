1434959cacf4f05e7ca5e9c8ebd76db8fad6a1b1
KaareSkytte <kaareskytte1431@gmail.com>
2025-07-11 13:33:23 +0200

    Added function declaration

--- main.py
+++ main.py
@@ -3,6 +3,23 @@ from dotenv import load_dotenv
 from google import genai
 import sys
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
+
+available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )   
+
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
 
 def main():
     load_dotenv()
@@ -30,24 +47,35 @@ def main():
 
     generate_content(client, messages)
 
-def generate_content(client, messages):
+def generate_content(client, messages):    
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
-        contents=messages
+        contents=messages,
+        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
     )
-    print("Response:")
-    print(response.text)
+    
+    if len(response.function_calls) != 0:
+        print(f"Calling function: {response.function_calls[0].name}({response.function_calls[0].args})")
+
+    else:
+        print("Response:")
+        print(response.text)
 
 def generate_content_verbose(client, messages):
     print(f"User prompt: {messages[0].parts[0].text}")
 
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
-        contents=messages
+        contents=messages,
+        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
     )
+    
+    if len(response.function_calls) != 0:
+        print(f"Calling function: {response.function_calls[0].name}({response.function_calls[0].args})")
 
-    print("Response:")
-    print(response.text)
+    else:
+        print("Response:")
+        print(response.text)
 
     prompt_tokens_used = response.usage_metadata.prompt_token_count
     response_tokens_used = response.usage_metadata.candidates_token_count
